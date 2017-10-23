dfb1128baadc6e1e7721139066f62935c2bdc1f9
bcompter <bcompter@gmail.com>
2017-10-23 15:57:14 -0400

    Updated script

--- Pi/monster_mash.py
+++ Pi/monster_mash.py
@@ -1,4 +1,4 @@
-import pygame, time, serial
+import pygame, time, serial, math
 from pygame.locals import *
 
 # Load music file into pygame
@@ -16,28 +16,42 @@ fo = open("commands.csv", "r")
 pygame.mixer.music.play()
 
 # Loop until the music is over
-while (pygame.mixer.music.get_pos() < 10000):
-	
-	# testing...
-	time.sleep(1)
+while (True):
 
 	# Read and configure the next command from the script
 	command = fo.readline()
-	bytes = command.split(",")
-	time_to_send = float(bytes[0])
-	msg[0] = byte(bytes[1])
-	msg[1] = byte(bytes[2])
-	msg[2] = byte(bytes[3])
-	msg[3] = byte(bytes[4])
-	msg[4] = msg[0] + msg[1] + msg[2] + msg[3]
 	
-	# Sleep while we are not ready for the command
+	# Check for the end of commands
+	if (command == "")
+		break
+	
+	# Split input into the component bytes
+	command_bytes = command.split(",")
+	time_to_send = float(command_bytes[0])
+	
+	# Skip the header
+	if ( math.isnan(time_to_send) ):
+		continue
+	
+	# Create our command bytes to send
+	msg[0] = bytes(command_bytes[1])			# Command / ID
+	msg[1] = bytes(command_bytes[2])			# Payload 1
+	msg[2] = bytes(command_bytes[3])			# Payload 2
+	msg[3] = bytes(command_bytes[4])			# Payload 3
+	msg[4] = msg[0] + msg[1] + msg[2] + msg[3]	# Checksum
+	
+	# Wait until the next command needs to be sent
 	while (pygame.mixer.music.get_pos() < time_to_send):
-		time.sleep(1)
+		pass		# Busy wait...
 		
 	# Send new command over serial (twice to be sure to be sure...)
 	serialPort.write(msg)
 
-
+# Fade out
+pygame.mixer.music.fadeout(1500)
+	
 # Close our script file
+fo.close()
 
+# Play it again!
+# todo...
