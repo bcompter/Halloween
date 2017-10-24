8583416aa925579e125b78771a4a601bedb50f22
bcompter <bcompter@gmail.com>
2017-10-23 23:11:32 -0400

    Updated python script, added testing script

--- Pi/monster_mash.py
+++ Pi/monster_mash.py
@@ -1,5 +1,20 @@
-import pygame, time, serial, math
+import pygame, time, serial, math, codecs
 from pygame.locals import *
+
+# Test for a float
+def isfloat(value):
+	try:
+		float(value)
+		return True
+	except:
+		return False
+
+# Convert integer to a byte
+def int2bytes(i):
+    hex_value = '{0:x}'.format(i)
+    # make length of hex_value a multiple of two
+    hex_value = '0' * (len(hex_value) % 2) + hex_value
+    return codecs.decode(hex_value, 'hex_codec')
 
 # Load music file into pygame
 pygame.init()
@@ -10,7 +25,8 @@ serialPort = serial.Serial("/dev/ttyAMA0", 19200)
 msg = bytearray(5)
 
 # Open script for reading
-fo = open("commands.csv", "r")
+#fo = open("commands.csv", "r")
+fo = open("testing.csv", "r")
 
 # Start the music
 pygame.mixer.music.play()
@@ -21,27 +37,31 @@ while (True):
 	# Read and configure the next command from the script
 	command = fo.readline()
 	
+	print("Command is: %s", command)
+	
 	# Check for the end of commands
-	if (command == "")
+	if (command == ""):
 		break
 	
 	# Split input into the component bytes
 	command_bytes = command.split(",")
-	time_to_send = float(command_bytes[0])
 	
 	# Skip the header
-	if ( math.isnan(time_to_send) ):
+	if ( isfloat(command_bytes[0]) != True ):
 		continue
-	
+
 	# Create our command bytes to send
-	msg[0] = bytes(command_bytes[1])			# Command / ID
-	msg[1] = bytes(command_bytes[2])			# Payload 1
-	msg[2] = bytes(command_bytes[3])			# Payload 2
-	msg[3] = bytes(command_bytes[4])			# Payload 3
-	msg[4] = msg[0] + msg[1] + msg[2] + msg[3]	# Checksum
+	time_to_send = float(command_bytes[0])
+	
+	msg[0] = int2bytes(int(command_bytes[1]))	# Command / ID
+	msg[1] = int2bytes(int(command_bytes[2]))	# Payload 1
+	msg[2] = int2bytes(int(command_bytes[3]))	# Payload 2
+	msg[3] = int2bytes(int(command_bytes[4]))	# Payload 3
+	checksum = msg[0] + msg[1] + msg[2] + msg[3]
+	msg[4] = checksum % 256
 	
 	# Wait until the next command needs to be sent
-	while (pygame.mixer.music.get_pos() < time_to_send):
+	while (pygame.mixer.music.get_pos() < time_to_send*1000):
 		pass		# Busy wait...
 		
 	# Send new command over serial (twice to be sure to be sure...)
